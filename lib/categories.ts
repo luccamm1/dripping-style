7ff048a1c1701aabce4549b160f0630aebd88c9d
luccas <lukasmedina1721@gmail.com>
2026-05-27 12:46:01 -0300

    add back pantalones category

--- lib/categories.ts
+++ lib/categories.ts
@@ -4,5 +4,6 @@ export const categories: Category[] = [
   { id: "1", name: "Remeras", slug: "remeras" },
   { id: "2", name: "Hoodies", slug: "hoodies" },
   { id: "3", name: "Shorts", slug: "shorts" },
-  { id: "4", name: "Zapatillas", slug: "zapatillas" },
+  { id: "4", name: "Pantalones", slug: "pantalones" },
+  { id: "5", name: "Zapatillas", slug: "zapatillas" },
 ];
